9d13cb58345919ac79d36330caaa1c92eb18d95c
Alexandre Campino <alexanderluiscampino@gmail.com>
2018-02-02 11:58:28 -0800

    - Complete Structure redifinion
    - Change of file names
    - change of folder structure

--- run.bat
+++
@@ -1,7 +0,0 @@
-echo off
-title Create Files DMs and Gifts
-:: See the title at the top. And this comment will not appear in the command prompt.
-cd %cd%\Desktop\Cabalidays
-python cabalidays.py
-
-pause
